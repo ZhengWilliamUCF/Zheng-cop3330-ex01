e7c3d79084dce246bf86dd4e3916297b145e4a22
William <ZhengWilliam@knights.ucf.edu>
2021-06-02 17:58:26 -0400

    Changes

--- src/main/java/Exercise1/App.java
+++ src/main/java/Exercise1/App.java
@@ -3,18 +3,26 @@ package Exercise1;
 import java.util.Scanner;
 
 public class App {
+    static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        App myApp = new App();
+        String name = myApp.readName();
+        String outputString = myApp.generateOutputString(name);
+        myApp.printOutput(outputString);
+    }
 
-        // Input
-        System.out.print("What is your name? ");
-        String name = in.nextLine();
-
-        // Concat
-        String outputString = "Hello, " + name + ", nice to meet you!";
-
-        // Output
+    public void printOutput(String outputString) {
         System.out.println(outputString);
     }
 
+    public String readName() {
+        System.out.print("What is your name? ");
+        String name = in.nextLine();
+        return name;
+    }
+
+    public String generateOutputString(String name){
+        return String.format("Hello, %s, nice to meet you!", name);
+    }
 }

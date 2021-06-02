a88f0f46f419846c1bdc23ada6510da7ddb91e0c
William <ZhengWilliam@knights.ucf.edu>
2021-06-02 17:20:11 -0400

    Changes

--- src/main/java/Exercise1/App.java
+++ src/main/java/Exercise1/App.java
@@ -1,6 +1,20 @@
 package Exercise1;
 
+import java.util.Scanner;
+
 public class App {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // Input
+        System.out.print("What is your name? ");
+        String name = in.nextLine();
+
+        // Concat
+        String outputString = "Hello, " + name + ", nice to meet you!";
+
+        // Output
+        System.out.println(outputString);
+    }
 
 }
